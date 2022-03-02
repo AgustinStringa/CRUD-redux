b9394dd5c5753e261b9ddc5b91f689b1c6bdd191
Agustín Stringa <stringaagustin1@gmail.com>
2022-03-02 16:49:26 -0300

    header instructor version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,17 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <nav className="navbar bg-success">
-        <h1 className="text-white p-3 ">
-          CRUD - React, Redux, REST API & Axios
-        </h1>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-rpimary justify-content-between">
+        <div className="container ">
+          <h1 className="text-white p-3 ">
+            CRUD - React, Redux, REST API & Axios
+          </h1>
+        </div>
 
-        <a href="/productos/nuevo" className="btn btn-danger nuevo-post p-3">
+        <a
+          href="/productos/nuevo"
+          className="btn btn-danger nuevo-post d-block d-md-block"
+        >
           Agregar Producto +
         </a>
       </nav>
