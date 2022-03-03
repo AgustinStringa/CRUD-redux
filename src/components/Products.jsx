ed037e51bdbe5231791ee6012301f9c90674714c
Agustín Stringa <stringaagustin1@gmail.com>
2022-03-03 00:20:27 -0300

    making table products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div className="row justify-content-center my-5">
+      <h2 className="text-center text-uppercase">Product List</h2>
+
+      <table className="table table-stripped">
+        <thead className="table-dark bg-primary">
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+        <tfoot className="table-dark bg-primary">
+          <tr className="font-weight-bold">
+            <td>Name</td>
+            <td>Price</td>
+            <td>Actions</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
 };
 
 export default Products;
